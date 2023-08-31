68f57f4b5767436fec52621318054939ec0a6033
gianellaromero <romerogianellaa@gmail.com>
2023-08-31 19:25:48 -0300

    cambios

---
+++ database/migrations/2023_08_31_215919_create_tutors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tutors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('lastname');
+            $table->string('email');
+            $table->string('phone_number');
+            $table->string('dni');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tutors');
+    }
+};
