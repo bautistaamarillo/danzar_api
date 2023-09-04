22e63887e0f5249462b5a98c11d6e37396499664
gianellaromero <romerogianellaa@gmail.com>
2023-09-04 17:44:19 -0300

    cambios

---
+++ database/migrations/2023_09_04_203057_create_student_tutors_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('student_tutors', function (Blueprint $table) {
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('tutor_id');
+            $table->primary(['student_id' , 'tutor_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('student_tutors');
+    }
+};
