8cba9406e559ca0929016d1e517e5ea18de51f95
gabriel lapeire <lapeiregabriel@gmail.com>
2023-09-07 18:54:27 -0300

    ItemStudentSeeder en DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,7 +23,7 @@ class DatabaseSeeder extends Seeder
             CategorySeeder::class,
             PaymentSeeder::class,
             StudentTutorSeeder::class,
-            StudentItemSeeder::class
+            ItemStudentSeeder::class
         ]);
         
     }
