f52aa52502996d1a7b687ecca3ce0e6ccd6afa0a
bautistaamarillo <amarillobautista5@gmail.com>
2023-09-13 20:01:53 -0300

    route test payment

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TutorController;
+use App\Models\Payment;
 
 
 Route::get('/', function () {
