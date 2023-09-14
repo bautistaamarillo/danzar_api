18b3cd6554a72893480436fbfa6084960b384661
gianellaromero <romerogianellaa@gmail.com>
2023-09-14 19:16:07 -0300

    relaciones hechas

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Payment extends Model
 {
@@ -15,4 +16,5 @@ class Payment extends Model
         'date',
         'invoice_number',
     ];
+    
 }
