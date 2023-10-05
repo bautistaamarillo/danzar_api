e6d4e2b4b5d4a320876f1ca37ccd9a71c7a4dab6
bautistaamarillo <amarillobautista5@gmail.com>
2023-10-05 18:36:25 -0300

    comente los cors

--- routes/api.php
+++ routes/api.php
@@ -3,22 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    //  header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header("Access-Control-Allow-Origin: *");
-    // header('Access-Control-Allow-Credentials: true');
-    // header('Access-Control-Max-Age: 86400');
-    }
-    // header('Content-Type: application/json; charset=utf-8');
+// if (isset($_SERVER['HTTP_ORIGIN'])) {
+     //header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+   //  header("Access-Control-Allow-Origin: *");
+  //   header('Access-Control-Allow-Credentials: true');
+//     // header('Access-Control-Max-Age: 86400');
+//     }
+//     // header('Content-Type: application/json; charset=utf-8');
     
-    // if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    // if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-    // header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    // }
-    // if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
-    // header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+//     // if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+//     // if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
+//     // header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+//     // }
+//     // if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
+//     // header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
     
-    die();
+//     die();
     
 
 /*
