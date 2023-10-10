a6df08e51e74233f10513996a4ea486460729d65
gabriel lapeire <lapeiregabriel@gmail.com>
2023-10-10 19:47:55 -0300

    use validator

--- app/Http/Controllers/SettingController.php
+++ app/Http/Controllers/SettingController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Setting;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class SettingController extends Controller
 {
--- app/Http/Controllers/TutorController.php
+++ app/Http/Controllers/TutorController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Tutor;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TutorController extends Controller
 {
