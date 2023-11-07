407fd052f8f8a841df1ef55c8c548bf08775e138
gabriel lapeire <lapeiregabriel@gmail.com>
2023-11-07 20:35:11 -0300

    cambio chiquito a item seeder

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -16,31 +16,31 @@ class ItemSeeder extends Seeder
         DB::table('items')->insert([
             'name'=> 'arabe',
             'amount'=> 2000,
-            'category_id'=> 1,
+            'category_id'=> 2,
             'active' => 1
         ]);
         DB::table('items')->insert([
             'name'=> 'clásico',
             'amount'=> 3000,
-            'category_id'=> 1,
+            'category_id'=> 2,
             'active' => 1
         ]);
         DB::table('items')->insert([
             'name'=> 'brasilero',
             'amount'=> 2500,
-            'category_id'=> 1,
+            'category_id'=> 2,
             'active' => 1
         ]);
         DB::table('items')->insert([
             'name'=> 'contemporaneo',
             'amount'=> 3000,
-            'category_id'=> 1,
+            'category_id'=> 2,
             'active' => 1
         ]);
         DB::table('items')->insert([
             'name'=> 'hip-hop',
             'amount'=> 2500,
-            'category_id'=> 1,
+            'category_id'=> 2,
             'active' => 1
         ]);
     }
