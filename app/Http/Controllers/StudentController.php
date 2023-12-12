46df40d99c73553f5526e5380848bf8e49855683
Rebrit Users <rbtusers@rebrit.ar>
2023-12-12 16:02:40 -0300

    student show restaurado, student edit

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -91,11 +91,11 @@ class StudentController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id,)
+    public function show(string $id)
     {
-        $student = Student::WHERE('ID', $id)
-        ->with('items')->get();
-        return $student;
+        $student = Student::WHERE('ID', $id)->get();
+        
+        return $student[0]->items;
     }
 
     /**
@@ -103,7 +103,8 @@ class StudentController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $student = Student::WHERE('ID', $id)->get();
+        return $student;
     }
 
     /**
